2f83e27eeffc0ec5dc66f1d107a3efdbb710541a
Ross Brandon <ross@blackbird.lan>
2020-11-25 00:29:19 -0600

    Updates

---
+++ precache-manifest.403de04b980318ca1e679d6e026cb9c7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a731570067517082a1a1ed8da1d3c3c",
+    "url": "/rebekahandross.github.io/index.html"
+  },
+  {
+    "revision": "b8d136c9aac511517ac9",
+    "url": "/rebekahandross.github.io/static/css/2.dfc52a7f.chunk.css"
+  },
+  {
+    "revision": "d53024943c17573b2dec",
+    "url": "/rebekahandross.github.io/static/css/main.7affb9be.chunk.css"
+  },
+  {
+    "revision": "b8d136c9aac511517ac9",
+    "url": "/rebekahandross.github.io/static/js/2.5587335d.chunk.js"
+  },
+  {
+    "revision": "d53024943c17573b2dec",
+    "url": "/rebekahandross.github.io/static/js/main.b89c21ba.chunk.js"
+  },
+  {
+    "revision": "bd1221d991854a745ea6",
+    "url": "/rebekahandross.github.io/static/js/runtime~main.b57a192d.js"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/rebekahandross.github.io/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rebekahandross.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rebekahandross.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rebekahandross.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rebekahandross.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rebekahandross.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/rebekahandross.github.io/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/rebekahandross.github.io/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/rebekahandross.github.io/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/rebekahandross.github.io/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "265f64ef84d26c41c7b4d17aa57864dd",
+    "url": "/rebekahandross.github.io/static/media/vector.265f64ef.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f641519c12f0a07279201f0df190858c.js"
+  "/rebekahandross.github.io/precache-manifest.403de04b980318ca1e679d6e026cb9c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rebekahandross.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
