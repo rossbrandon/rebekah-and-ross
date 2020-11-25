7e83b78ace03b04c8f3db0cefd564883aad9171a
Ross Brandon <ross@blackbird.lan>
2020-11-25 00:24:20 -0600

    Updates

---
+++ precache-manifest.f641519c12f0a07279201f0df190858c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "040dce2191d36f561fb8fa500adb4504",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b996fbafa446884798b",
+    "url": "/static/css/2.467c85df.chunk.css"
+  },
+  {
+    "revision": "bb369bf64e0b658c07cb",
+    "url": "/static/css/main.9d2f0bbe.chunk.css"
+  },
+  {
+    "revision": "3b996fbafa446884798b",
+    "url": "/static/js/2.831d056a.chunk.js"
+  },
+  {
+    "revision": "bb369bf64e0b658c07cb",
+    "url": "/static/js/main.b82e0e5d.chunk.js"
+  },
+  {
+    "revision": "8b504118f65ba8d01b31",
+    "url": "/static/js/runtime~main.0772025e.js"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "265f64ef84d26c41c7b4d17aa57864dd",
+    "url": "/static/media/vector.265f64ef.png"
+  }
+]);
