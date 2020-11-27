f179483504ce15d686dfbd4385f2a73b6805af28
Ross Brandon <rosstafarian1@gmail.com>
2020-11-27 01:36:31 -0600

    Lots of new text for our story

--- clear_cloudflare_cache.sh
+++ clear_cloudflare_cache.sh
@@ -32,6 +32,6 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 # delete cloudflare cache
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache" \
      -H "X-Auth-Email: $CLOUDFLARE_AUTH_EMAIL" \
-     -H "X-Auth-Key: $CLOUDFLARE_AUTH_KEY" \
+     -H "Authorization: Bearer $CLOUDFLARE_AUTH_KEY" \
      -H "Content-Type: application/json" \
      --data '{"purge_everything":true}'
--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import SectionTitle from "../SectionTitle"
 import story1 from "../../images/story/dating.jpg"
-import story2 from "../../images/story/engagement-2.jpg"
+import story2 from "../../images/story/zachs-wedding.jpg"
+import story3 from "../../images/story/engagement.jpg"
 import "./style.css"
 
 const Story = () => {
@@ -11,29 +12,62 @@ const Story = () => {
             <div className="container">
                 <div className="story-wrap">
                     <div className="row">
-                        <div className="col-lg-6 col-md-12 col-12 pr-n">
+                        <div className="col-lg-6 col-md-12 col-12 pl-n">
                             <div className="story-img">
                                 <img src={story1} alt="" />
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-12 col-12">
                             <div className="story-text left-align-text">
-                                <h3>Our Second First Date</h3>
-                                <span className="date">June 3, 2016</span>
+                                <h3>How We Met</h3>
+                                <span className="date">March ?, 2016</span>
                                 <p>
-                                    Rebekah and Ross did not start officially
-                                    dating after their first date. It was their
-                                    SECOND first date that stuck.
+                                    Ross is an Austin tech-scene transplant from
+                                    Fayetteville, AR and Rebekah is an tried and
+                                    true Austinite. At the time Ross lived in
+                                    the Domain. He and a few friends were
+                                    relaxing at the local Irish pub when a
+                                    mutual friend connected our groups. Ross was
+                                    immediately struck by Rebekah and asked her
+                                    out that night.
                                     <br />
                                     <br />
-                                    Little did they know that one date would
-                                    turn into two. One year of dating into four
-                                    and a proposal for a lifetime.{" "}
+                                    (Rebekah needs to inject her side here)
                                 </p>
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-12 col-12">
                             <div className="story-text right-align-text">
+                                <h3>Our Second First Date</h3>
+                                <span className="date">June 3, 2016</span>
+                                <p>
+                                    Rebekah and Ross did not officially start
+                                    dating after their first date. It was their
+                                    SECOND first date that stuck.
+                                    <br />
+                                    <br />
+                                    They quickly realized that this was
+                                    something real. They had the most fun
+                                    together, encouraged and made each other
+                                    better and sure enough... that second date
+                                    would turn into a third; one year of dating
+                                    into four and a proposal to turn that into a
+                                    lifetime.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-12 col-12 pr-n">
+                            <div className="story-img">
+                                <img src={story2} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-12 col-12 pl-n">
+                            <div className="story-img">
+                                <img src={story3} alt="" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-12 col-12">
+                            <div className="story-text left-align-text">
                                 <h3>Our Engagement</h3>
                                 <span className="date">March 5, 2020</span>
                                 <p>
@@ -46,16 +80,21 @@ const Story = () => {
                                     Christmas. All with a hidden agenda...
                                     <br />
                                     <br />
-                                    After walking the Brooklyn Bridge with a
-                                    ring tightly in his grasp, Ross proposed
-                                    unexpectedly in the Brooklyn Bridge Park in
-                                    NYC!{" "}
+                                    After arguring over which deli had the best
+                                    bagels, they decided to walk across the
+                                    Brooklyn Bridge: Ross, with a ring tightly
+                                    in his grasp in his coat pocket and Rebekah
+                                    with a camera in hand ready for all the
+                                    selfie opportunities. The two made it to the
+                                    beautiful Brooklyn Bridge Park and Rebekah
+                                    wanted a picture. Ross put his coat on the
+                                    ground and setup his phone agaisnt it for a
+                                    timed picture. Except... he turned on the
+                                    video. Feigning a countdown for a picture,
+                                    Ross took Rebekah's hand, got down on one
+                                    knee, and asked her to be his best friend
+                                    and love of his life forever.
                                 </p>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 col-md-12 col-12 pl-n">
-                            <div className="story-img">
-                                <img src={story2} alt="" />
                             </div>
                         </div>
                     </div>
